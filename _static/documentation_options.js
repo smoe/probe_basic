a395c318d5e77a5a1f87299bf866823730479111
TurBoss <turboss@linuxcnc.lan>
2022-02-27 07:15:20 +0100

    deploy gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'py2+1.g7eca4f6.dirty',
+    VERSION: 'py2+2.g4938df3.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
